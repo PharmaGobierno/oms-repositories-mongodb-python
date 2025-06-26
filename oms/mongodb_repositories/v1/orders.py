748224afdf8e1e03ae5a03e64b0a8129e4a3306e
Luis <gerardo.fosado@cargamos.com>
2025-06-26 10:37:48 -0600

    feat(repositories): add orders remissions and events

---
+++ oms/mongodb_repositories/v1/orders.py
@@ -0,0 +1,5 @@
+from .base import BaseMongoDbRepository
+
+
+class OrdersRepository(BaseMongoDbRepository):
+    pass
---
+++ oms/mongodb_repositories/v1/remissions.py
@@ -0,0 +1,5 @@
+from .base import BaseMongoDbRepository
+
+
+class RemissionsRepository(BaseMongoDbRepository):
+    pass
